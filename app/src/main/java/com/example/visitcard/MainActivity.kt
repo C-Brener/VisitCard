124192bf611a2340a903857f193d3ef4e76f9c8e
Caíque Brener <brenercaique0806@gmail.com>
2022-11-28 22:31:41 -0300

    Create informations card

--- app/src/main/java/com/example/visitcard/MainActivity.kt
+++ app/src/main/java/com/example/visitcard/MainActivity.kt
@@ -5,14 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.visitcard.ui.theme.VisitCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +35,30 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun InformationsCard(icon: Painter, information: String) {
+    Column() {
+        Divider(color = Color.White)
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .align(Alignment.CenterHorizontally)) {
+            Icon(
+                painter = icon, contentDescription = null, modifier = Modifier
+                    .width(30.dp)
+                    .height(30.dp),
+                tint = colorResource(
+                    id = R.color.white
+                )
+            )
+            Text(text = information,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+
+
+}
 
 
 @Composable
@@ -56,7 +83,11 @@ fun SkeletonScreen() {
                 .fillMaxWidth()
                 .fillMaxHeight()
         ) {
-
+            Column(modifier = Modifier.padding(start = 10.dp, end = 10.dp).fillMaxHeight().wrapContentHeight(Alignment.CenterVertically)) {
+                InformationsCard(icon = painterResource(id = R.drawable.ic_baseline_call_24), information = "+55 (71) 98323-0048" )
+                InformationsCard(icon = painterResource(id = R.drawable.ic_baseline_share_24), information = "Android Developer" )
+                InformationsCard(icon = painterResource(id = R.drawable.ic_baseline_email_24), information = "brenercaique0806@gmail.com" )
+            }
         }
     }
 }
@@ -64,6 +95,6 @@ fun SkeletonScreen() {
 @Preview(showSystemUi = true)
 @Composable
 fun SkeletonScreenPreview() {
-    SkeletonScreen()
+   SkeletonScreen()
 }
 
