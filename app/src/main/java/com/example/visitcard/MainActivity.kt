af931a4952c665784c3500ead2d07944f6149cd9
Caíque Brener <brenercaique0806@gmail.com>
2022-11-28 21:58:28 -0300

    Create Skeleton Component

--- app/src/main/java/com/example/visitcard/MainActivity.kt
+++ app/src/main/java/com/example/visitcard/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.visitcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.visitcard.ui.theme.VisitCardTheme
 
@@ -22,10 +25,45 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    SkeletonScreen()
                 }
             }
         }
     }
+}
+
+
+
+@Composable
+fun SkeletonScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .background(color = colorResource(id = R.color.background_app))
+    ) {
+        Row(
+            modifier = Modifier
+                .weight(3f)
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+
+        }
+        Row(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun SkeletonScreenPreview() {
+    SkeletonScreen()
 }
 
